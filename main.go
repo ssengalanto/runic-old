38791b3330f7cbdb16ea913683a128100c68edf3
ssengalanto <ssengalanto.sde@gmail.com>
2023-07-11 20:36:43 +0800

    create initial API endpoint

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Welcome to Runic project!")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Welcome to Runic!"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", mux)) //nolint:gosec //todo
 }
