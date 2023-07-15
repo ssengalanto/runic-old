76d1dfd5c40b14a147ad31a96f33f4609b3c1bcd
ssengalanto <ssengalanto.sde@gmail.com>
2023-07-15 16:06:47 +0800

    create sentinel errors for config package

---
+++ services/account/internal/config/errors.go
@@ -0,0 +1,10 @@
+package config
+
+import "fmt"
+
+var (
+	// ErrReadConfigFileFailed is an error that indicates a failure to read the configuration file.
+	ErrReadConfigFileFailed = fmt.Errorf("failed to read config file")
+	// ErrReadEnvFailed is an error that indicates a failure to read environment variables.
+	ErrReadEnvFailed = fmt.Errorf("failed to read environment variabless")
+)
