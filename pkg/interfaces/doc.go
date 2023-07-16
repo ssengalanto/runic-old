f1d7761787daeedb57d2769030098b80f5e20996
ssengalanto <ssengalanto.sde@gmail.com>
2023-07-16 21:18:58 +0800

    create interfaces package

---
+++ pkg/interfaces/doc.go
@@ -0,0 +1,15 @@
+// Package interfaces provides the interface definitions used by the application.
+//
+// The interfaces package defines various interfaces that represent common behaviors and abstractions
+// used throughout the application. These interfaces serve as contracts that define the expected
+// methods and functionality for different components and can be implemented by concrete types.
+//
+// By using interfaces, the application achieves loose coupling, allowing different components to
+// interact and collaborate without having direct dependencies on each other. This promotes modular
+// design, code reusability, and easier testing and mocking of components.
+//
+// The interfaces defined in this package cover different aspects such as logging, configuration,
+// database access, external service integration, and more. These interfaces provide a flexible and
+// extensible foundation for the application's architecture, allowing different implementations
+// to be easily swapped and customized based on specific requirements.
+package interfaces
