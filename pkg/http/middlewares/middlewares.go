aec7a0396d2b335cdcde06e4a8cfc60bcd031227
ssengalanto <ssengalanto.sde@gmail.com>
2023-07-16 09:39:24 +0800

    update http middlewares package docs

--- pkg/http/middlewares/middlewares.go
+++ pkg/http/middlewares/middlewares.go
@@ -2,6 +2,8 @@ package middlewares
 
 import "net/http"
 
+// JSONContentType is a middleware that sets the "Content-Type" header of the response to "application/json".
+// It wraps the provided http.Handler and delegates the request/response processing to the next middleware or handler in the chain.
 func JSONContentType(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
