5d3cb2ea3990654e34d91e17f5777bce1fb86128
ssengalanto <ssengalanto.sde@gmail.com>
2023-07-16 09:38:24 +0800

    update constants package docs

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -1,16 +1,41 @@
-// Package constants provides constant values that are used throughout the application.
 package constants
 
 import "time"
 
 const (
-	MaxHeaderBytes       = 1 << 20
-	IdleTimeout          = time.Minute
-	ReadTimeout          = 10 * time.Second
-	WriteTimeout         = 30 * time.Second
-	RateLimit            = 100
-	Timeout              = 10 * time.Second
-	ShutdownGracePeriod  = 15 * time.Second
-	CorsMaxAge           = 300
+	// MaxHeaderBytes represents the maximum number of bytes allowed for HTTP headers.
+	MaxHeaderBytes = 1 << 20
+
+	// IdleTimeout represents the maximum duration of idle keep-alive connections.
+	IdleTimeout = time.Minute
+
+	// ReadTimeout represents the maximum duration for reading the entire request.
+	ReadTimeout = 10 * time.Second
+
+	// WriteTimeout represents the maximum duration for writing the entire response.
+	WriteTimeout = 30 * time.Second
+
+	// RateLimit represents the maximum number of requests allowed per second.
+	RateLimit = 100
+
+	// Timeout represents the default duration for request timeouts.
+	Timeout = 10 * time.Second
+
+	// ShutdownGracePeriod represents the duration to allow for graceful shutdown of the server.
+	ShutdownGracePeriod = 15 * time.Second
+
+	// CorsMaxAge represents the maximum duration to cache CORS (Cross-Origin Resource Sharing) responses.
+	CorsMaxAge = 300
+
+	// GzipCompressionLevel represents the compression level for Gzip compression.
 	GzipCompressionLevel = 5
+
+	// DevEnv represents the development environment.
+	DevEnv = "development"
+
+	// StgEnv represents the staging environment.
+	StgEnv = "staging"
+
+	// ProdEnv represents the production environment.
+	ProdEnv = "production"
 )
