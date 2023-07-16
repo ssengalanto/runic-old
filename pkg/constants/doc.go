31a53aef098c6dcb86407e12de5ddc1926c80154
ssengalanto <ssengalanto.sde@gmail.com>
2023-07-16 09:38:24 +0800

    update constants package docs

---
+++ pkg/constants/doc.go
@@ -0,0 +1,10 @@
+// Package constants provides constant values that are used throughout the application.
+//
+// The constants package defines various constant values that are used across the application.
+// These constants represent configuration settings, default values, limits, and other fixed values
+// that are used by different components of the application.
+//
+// The constants in this package are intended to provide a centralized location for managing and
+// accessing these values, making it easier to maintain and update them consistently throughout
+// the codebase.
+package constants
