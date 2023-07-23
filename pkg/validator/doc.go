69a8e55d0cea230b879b16fd67e88219468cd1fe
ssengalanto <ssengalanto.sde@gmail.com>
2023-07-23 21:04:54 +0800

    create internal validator package

---
+++ pkg/validator/doc.go
@@ -0,0 +1,5 @@
+// Package validator provides utilities for validating data using struct field tags
+// and custom validation rules. It uses the "go-playground/validator/v10" package to
+// perform the validation. The package offers functions for both single field
+// validation and struct validation.
+package validator
